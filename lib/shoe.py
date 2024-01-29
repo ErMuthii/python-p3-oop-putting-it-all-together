782ac0402869614aac459c380400b97840419b11
ErMuthii <er.muthii@gmail.com>
2024-01-29 22:23:43 +0300

    done

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
+from typing import Any
+
+
 class Shoe:
-    pass
+    
+    def __init__(self,brand,size):
+        self.brand = brand
+        self._size = size
+
+    def get_size(self):
+        print('Retrieving size')
+        return self._size 
+    
+    def set_size(self,size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print('size must be an integer')
+
+    def cobble(self):
+        print('Your shoe is as good as new!')
+        self.condition = 'New'
+    
+
+        
+    size = property(get_size,set_size)
